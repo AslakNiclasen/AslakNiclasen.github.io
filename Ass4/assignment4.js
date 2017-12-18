6d5d0937fd2a0703bf25af0a553dcea63e45c1db
AslakNiclasen <aslak.niclasen@gmail.com>
2017-12-18 15:51:09 +0100

    updated plots

--- Ass4/assignment4.js
+++ Ass4/assignment4.js
@@ -1,7 +1,7 @@
 // The line plot
 var margin = {top: 50, right: 20, bottom: 50, left: 50},
-    width = 700 - margin.left - margin.right,
-    height = 700 - margin.top - margin.bottom;
+    width = 500 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 var x = d3.scaleLinear().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
@@ -15,7 +15,7 @@ var line = d3.line()
     });
 
 
-var svg1 = d3.select("body").append("svg")
+var svg1 = d3.select("#graphs").append("svg")
     .attr("id", "svg1")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -145,19 +145,19 @@ d3.text("hands.csv", function(error, text) {
 
 });
 
-var div2 = d3.select("body").append("div")
+var div2 = d3.select("#graphs").append("div")
     .attr("class", "legend1div");
 
-var div = d3.select("body").append("div")
+var div = d3.select("#graphs").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
 var margin2 = {top: 50, right: 20, bottom: 50, left: 50},
-    width2 = 1000 - margin2.left - margin2.right,
-    height2 = 700 - margin2.top - margin2.bottom;
+    width2 = 800 - margin2.left - margin2.right,
+    height2 = 500 - margin2.top - margin2.bottom;
 
 
-var svg2 = d3.select("body").append("svg")
+var svg2 = d3.select("#graphs").append("svg")
     .attr("id", "svg2")
     .attr("width", 700 + margin2.left + margin2.right)
     .attr("height", height2 + margin2.top + margin2.bottom)
@@ -165,12 +165,16 @@ var svg2 = d3.select("body").append("svg")
     .attr("transform",
         "translate(" + margin2.left + "," + margin2.top + ")");
 
+var hands_pca = [];
+var currentPCx = 0;
+var currentPCy = 1;
+
 // Get the data
 d3.text("hands_pca.csv", function(error, text) {
     if (error) {
         console.log("error");
     } else {
-        var hands_pca = d3.csvParseRows(text, function(d) {
+            hands_pca = d3.csvParseRows(text, function(d) {
             var hand = [];
             for (var i = 0; i < 112; i++) {
                 hand.push(+d[i]);
@@ -179,12 +183,19 @@ d3.text("hands_pca.csv", function(error, text) {
         });
     }
 
-    var first_two_pc = [];
+
+
+    var first_two_pc = getPCs(0,1);
+    
+
+
+    /*
     for (var i=0; i<40; i++) {
         var first = hands_pca[i][0];
         var second = hands_pca[i][1];
         first_two_pc.push([first, second]);
     }
+    */
 
     console.log(first_two_pc);
     console.log(first_two_pc[0]);
@@ -205,6 +216,9 @@ d3.text("hands_pca.csv", function(error, text) {
         .data(first_two_pc)
         .enter().append("circle")
         .attr("class", "dot")
+        .attr("stroke", "lightblue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none")
         .attr("r", 8)
         .attr("cx", function(d) { return x(d[0]); })
         .attr("cy", function(d) { return y(d[1]); })
@@ -219,10 +233,11 @@ d3.text("hands_pca.csv", function(error, text) {
     // text label for the x axis
     svg2.append("text")
         .attr("transform",
-            "translate(" + (width2/2) + " ," +
+            "translate(" + (450/2) + " ," +
             (height2 + 45) + ")")
         .style("text-anchor", "middle")
         .style("font-size", "24px")
+        .attr("id", "xLabel")
         .text("Principal Component 1");
 
     // Add the Y Axis
@@ -237,6 +252,7 @@ d3.text("hands_pca.csv", function(error, text) {
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .style("font-size", "24px")
+        .attr("id", "yLabel")
         .text("Principal Component 2");
 
 });
@@ -271,18 +287,14 @@ function updatePlot1(d, i) {  // Add interactivity
 }
 
 
-
-
-
-
-
-
 function updatePlot2(d, i) {  // Add interactivity
     x.domain([0, 1.2]);
     y.domain([0, 1.2]);
     // Use D3 to select element, change color and size
     d3.select(this)
-        .attr("fill", "black")
+        .attr("stroke", "lightblue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none")
         .call(function(d) {
             div.transition()
                 .duration(500)
@@ -296,6 +308,8 @@ function updatePlot2(d, i) {  // Add interactivity
         .style("opacity", .9)
         .style("fill", "black");
 
+
+
 }
 
 
@@ -307,10 +321,110 @@ function updatePlot2(d, i) {  // Add interactivity
 
 function update(i) {
 
-
-    d3.select("#svg1")
+    d3.select("#graphs")
         .selectAll('.line')
         .transition()
         .attr("d", line(hands8[i]));
 
+}
+
+/* Slider for X-axis*/
+var slider = d3.sliderHorizontal()
+               .min(1)
+               .max(40)
+               .step(1)
+               .width(500)
+               .on('onchange', val => {
+                    /* update plot */
+                    updateScatterX(val);
+               })
+
+var g = d3.select("div#slider2").append("svg")
+          .attr("width", 600)
+          .attr("height", 70)
+          .append("g")
+          .attr("transform", "translate(75,30)");
+
+g.call(slider);
+
+
+
+/*Slider for Y-axis */
+var sliderY = d3.sliderHorizontal()
+               .min(1)
+               .max(40)
+               .step(1)
+               .width(500)
+               .on('onchange', val => {
+                    /* update plot */
+                    updateScatterY(val);
+               })
+
+var gY = d3.select("div#slider2Y").append("svg")
+          .attr("width", 600)
+          .attr("height", 70)
+          .append("g")
+          .attr("transform", "translate(75,30)");
+
+gY.call(sliderY);
+
+
+
+
+function updateScatterX(val) {
+    var newPCs = getPCs(val-1, currentPCy);
+    currentPCx = val-1;
+    // Scale the range of the data
+    x.domain([-0.65, 0.65]);
+    //y.domain(d3.extent(hands_pca, function(d) { return d[1]; }));
+    y.domain([-0.65, 0.65]);
+
+    svg2.selectAll("circle")
+      .data(newPCs)
+      .transition()
+      .duration(1000)
+      .attr("cx", function(d) { return x(d[0]); } )
+      .attr("cy", function(d) { return y(d[1]); } )
+      .attr("r", 8)
+      .attr("stroke", "lightblue")
+      .attr("stroke-width", 2)
+      .attr("fill", "none")
+
+    d3.select("#xLabel").text("Principal Component " + val);
+}
+
+
+function updateScatterY(val) {
+    var newPCs = getPCs(currentPCx, val-1);
+    currentPCy = val-1;
+    // Scale the range of the data
+    x.domain([-0.65, 0.65]);
+    //y.domain(d3.extent(hands_pca, function(d) { return d[1]; }));
+    y.domain([-0.65, 0.65]);
+
+    svg2.selectAll("circle")
+      .data(newPCs)
+      .transition()
+      .duration(1000)
+      .attr("cx", function(d) { return x(d[0]); } )
+      .attr("cy", function(d) { return y(d[1]); } )
+      .attr("r", 8)
+      .attr("stroke", "lightblue")
+      .attr("stroke-width", 2)
+      .attr("fill", "none")
+
+    d3.select("#yLabel").text("Principal Component " + val);
+}
+
+
+
+function getPCs(pc1,pc2) {
+
+    res = [];
+    for (var i=0; i<40; i++) {
+        var first = hands_pca[i][pc1];
+        var second = hands_pca[i][pc2];
+        res.push([first, second]);
+    }
+    return res;
 }
